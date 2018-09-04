42c19aa59076f5df25c26f67921f9650cc27631f
DIR-IT <DIR-IT@DIR-IT-HP>
2018-09-04 15:44:13 -0500

    Proyecto Granja-JAVA DOC

---
+++ Granja/src/neta/systems/granja/Parcela.java
@@ -0,0 +1,21 @@
+package neta.systems.granja;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Parcela
+{
+	private static Queue<Maiz> parcela = new ArrayDeque<>(); //inicializacion de singletons
+	
+	public synchronized static void addMaiz(Maiz maiz) {
+		parcela.add(maiz);
+	}
+	
+	public synchronized static Maiz getMaiz() {
+		if(parcela.isEmpty()) {
+			return null;
+		}
+		return parcela.poll();
+	}
+	
+}
